e9c63dad5a95e278df2225b1969ef290965eab98
up637415 <up637415@myport.ac.uk>
2016-02-11 18:22:56 +0000

    Initial Commit

---
+++ Car Health CheckTests/Car_Health_CheckTests.swift
@@ -0,0 +1,36 @@
+//
+//  Car_Health_CheckTests.swift
+//  Car Health CheckTests
+//
+//  Created by Simon Wild on 11/02/2016.
+//  Copyright © 2016 Simon Wild. All rights reserved.
+//
+
+import XCTest
+@testable import Car_Health_Check
+
+class Car_Health_CheckTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
