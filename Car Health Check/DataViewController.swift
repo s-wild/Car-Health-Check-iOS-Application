7a727f7fb89e2ec25f551a339d87de37b7b88741
up637415 <up637415@myport.ac.uk>
2016-02-25 20:58:53 +0000

    Added more buttons and JSON calls for car parts.

--- Car Health Check/DataViewController.swift
+++ Car Health Check/DataViewController.swift
@@ -15,22 +15,35 @@ class DataViewController: UIViewController {
     var dataObject: String = ""
     @IBOutlet weak var dataLabel: UILabel!
     @IBOutlet weak var frontBrake: UIButton!
+    @IBOutlet weak var shockAbsorberButton: UIButton!
+    @IBOutlet weak var frontRightBrake: UIButton!
+    @IBOutlet weak var frontLeftBrake: UIButton!
+    @IBOutlet weak var rearLeftBrake: UIButton!
+    @IBOutlet weak var rearRightBrake: UIButton!
+    @IBOutlet weak var rearShockAbsorber: UIButton!
+    @IBOutlet weak var rearLeftLight: UIButton!
+    @IBOutlet weak var rearRightLight: UIButton!
+    @IBOutlet weak var frontRightLight: UIButton!
+    @IBOutlet weak var frontLeftLight: UIButton!
+    @IBOutlet weak var catalyticConverter: UIButton!
     
+    var parts = [UIButton]();   // To store all status buttons for parts
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additionalsetup after loadin 
+        // Do any additionalsetup after loadin
         //the view, typically from a nib.
         view.bringSubviewToFront(frontBrake)
-        frontBrake.layer.cornerRadius = 16
-        updateTimer = NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: "callFunction", userInfo: nil, repeats: true)
         
-        //var front_brake_right = false;
-        //var front_brake_left = false;
-        // front_brake_right value.
-        // front_brake_left value.
-        // Get shock_absorber.
-        // Get catalytic_converter.
+        // Initialise parts array
+        parts = [self.frontBrake, self.shockAbsorberButton ,self.frontRightBrake, self.frontLeftBrake, self.frontLeftBrake, self.rearLeftBrake, self.rearRightBrake, self.rearShockAbsorber, self.rearLeftLight, self.rearRightLight, self.frontRightLight, self.frontLeftLight, self.catalyticConverter];
+        
+        // Set corner radius's for buttons for parts.
+        for part:UIButton in parts {
+            part.layer.cornerRadius = 16;
+        }
+        
+        updateTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "callFunction", userInfo: nil, repeats: true)
         
         // Get User ID
         let userID = NSUserDefaults.standardUserDefaults().objectForKey("User_ID") as! Int;
@@ -55,19 +68,23 @@ class DataViewController: UIViewController {
             let id = car["id"] as! Int;
             if (id == car_id) {
                 carObj = car;
-//                print(carObj);
-                // Get Front Brake.
+
                 let parts = carObj["parts"] as! [AnyObject]!;
-//                print(parts);
                 
+                // Shock Absorbers
                 var shock_absorber:AnyObject?;
+                
+                // Brakes
                 var front_left_brake:AnyObject?;
                 var front_right_brake:AnyObject?;
-                var cata_converter:AnyObject?;
-
+                var rear_left_brake:AnyObject?;
                 
+                var cata_converter:AnyObject?;
+ 
                 for p:AnyObject in parts {
                     let name = p["name"] as! String;
+                    print("++++");
+                    print(name);
                     if (name == "shock_absorber") {
                         shock_absorber = p;
                     }
@@ -76,27 +93,30 @@ class DataViewController: UIViewController {
                     }
                     else if(name == "front_brake_right") {
                         front_right_brake = p;
-                    } else {
+                    }
+                    else if(name == "rear_brake_left") {
+                        rear_left_brake = p;
+                    }
+                    else {
                         cata_converter = p;
                     }
-                    
-                  //  print("---PARTS START");
-                    
-//                    print(front_left_brake);
-//                    print(front_right_brake);
-//                    print(cata_converter);
-                   // print("PARTS END---");
-                    
                 }
                 let shock_absorber_health = shock_absorber!["estimated_health_percentage"] as! Int;
                 toggleShockAbsorber(shock_absorber_health);
                 
                 
-                let front_brake_left_health = front_left_brake!["estimated_health_percentage"];
+                let front_brake_left_health = front_left_brake!["estimated_health_percentage"] as! Int;
+                toggleFrontLeftBrake (front_brake_left_health);
                 print("front_brake_left");
                 print(front_brake_left_health);
                 
-                let front_brake_right_health = front_right_brake!["estimated_health_percentage"];
+                let front_brake_right_health = front_right_brake!["estimated_health_percentage"] as! Int;
+                toggleFrontRightBrake(front_brake_right_health);
+                print("front_brake_right");
+                print(front_brake_right_health);
+                
+                let rear_brake_left_health = rear_left_brake!["estimated_health_percentage"] as! Int;
+                toggleFrontRightBrake(front_brake_right_health);
                 print("front_brake_right");
                 print(front_brake_right_health);
                 
@@ -105,8 +125,6 @@ class DataViewController: UIViewController {
                 print(catalytic_converter_health);
             }
         }
-        
-        
         
     }
 
@@ -127,21 +145,30 @@ class DataViewController: UIViewController {
     
     // Timer function
     func callFunction(){
+        UIView.animateWithDuration(1.0, animations: {
         
-        UIView.animateWithDuration(2.0, animations: {
             if (self.fade == 0) {
-                self.frontBrake.alpha = 0.3
+                for part:UIButton in self.parts {
+                    // Check if hidden
+                    if (!part.hidden) {
+                        part.alpha = 0.3;
+                    }
+                }
                 self.fade = 1
             }
             else {
-               self.frontBrake.alpha = 1
-                self.fade = 0
+                for part:UIButton in self.parts {
+                    // Check if hidden
+                    if (!part.hidden) {
+                        part.alpha = 1;
+                    }
+                }
+               self.fade = 0
             }
             
         })
     }
     
-    @IBOutlet weak var shockAbsorberButton: UIButton!
     // Set view for shock absorber
     func toggleShockAbsorber (shock_absorber_health:Int) {
         if (shock_absorber_health >= 70) {
@@ -164,9 +191,33 @@ class DataViewController: UIViewController {
         // Bad health < 50
             // Set red button and flash!
     }
-    
-    // Set
-    
+    func toggleFrontRightBrake(front_brake_right_health:Int) {
+        if (front_brake_right_health >= 70) {
+            frontRightBrake.hidden = true;
+            
+        } else if (front_brake_right_health > 50 && front_brake_right_health <= 70) {
+            frontRightBrake.hidden = false;
+            frontRightBrake.backgroundColor = UIColor(red: 255/255, green: 153/255, blue: 51/255, alpha: 1);
+            
+        } else if (front_brake_right_health < 50) {
+            frontRightBrake.hidden = false;
+            frontRightBrake.backgroundColor = UIColor(red: 233, green: 0, blue: 0, alpha: 1);
+        }
+    }
+    // Front Left Brake
+    func toggleFrontLeftBrake(front_brake_left_health:Int) {
+        if (front_brake_left_health >= 70) {
+            frontLeftBrake.hidden = true;
+            
+        } else if (front_brake_left_health > 50 && front_brake_left_health <= 70) {
+            frontLeftBrake.hidden = false;
+            frontLeftBrake.backgroundColor = UIColor(red: 255/255, green: 153/255, blue: 51/255, alpha: 1);
+            
+        } else if (front_brake_left_health < 50) {
+            frontLeftBrake.hidden = false;
+            frontLeftBrake.backgroundColor = UIColor(red: 233, green: 0, blue: 0, alpha: 1);
+        }
+    }
 
 }
 
